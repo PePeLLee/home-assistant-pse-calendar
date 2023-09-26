a9a4d789f4d11b4f552f7147570a4cfb99baed74
PePeLLee <61408245+PePeLLee@users.noreply.github.com>
2023-09-26 12:49:40 +0200

    Consider both "ZALECANE OSZCZĘDZANIE" and "WYMAGANE OGRANICZANIE"

--- custom_components/pse_calendar/calendar.py
+++ custom_components/pse_calendar/calendar.py
@@ -20,7 +20,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entitie
     """Konfiguracja za pomcą przepływu konfiguracji."""
     
     """This one is in use"""
-    async_add_entities([PSECalendar("ZALECANE OSZCZ", "PSE Oszczedzanie"), 
+    async_add_entities([PSECalendar("NE O", "PSE Oszczedzanie"), 
                         PSECalendar("ZALECANE U", "PSE Uzywanie")])
 
 
